34c2431de6c8e077f0757c53628ba6a0bb3d2408
Eduardo Santos <eduardojes72@gmail.com>
2021-05-11 08:59:29 -0300

    added the settings controller

--- src/controllers/SettingsController.ts
+++ src/controllers/SettingsController.ts
@@ -1,25 +1,42 @@
 import { Request, Response} from "express";
-import { getCustomRepository } from "typeorm";
-import { SettingsRepository } from "../repositories/SettingsRepository";
+import { SettingsService } from "../services/SettingsService";
 
 class SettingsController {
     async create(request: Request, response: Response){
         try{
             const {chat, username} = request.body;
-    
-            const settingsRepository = getCustomRepository(SettingsRepository)
-        
-            const settings = settingsRepository.create({
-                chat,
-                username,
-            });
-        
-            await settingsRepository.save(settings)
+            
+            const settingsService = new SettingsService();
+           
+            const settings = await settingsService.create({chat, username})
         
             return response.json(settings);
-        }catch(e){
-            return response.json(e);
+        }catch(err){
+            return response.status(400).json({
+                message: err.message
+            });
         }
+    }
+
+    async findByUsername(request: Request, response: Response){
+        const { username } = request.params
+
+        const settingsService = new SettingsService()
+
+        const settings = await settingsService.findByUserName(username)
+
+        return response.json(settings)
+    }
+
+    async update(request: Request, response: Response){
+        const { username } = request.params
+        const { chat } = request.body
+
+        const settingsService = new SettingsService()
+
+        const settings = await settingsService.update(username, chat)
+
+        return response.json(settings)
     }
 }
 
